091c60ef56668249d47a387e48dc9f2d4b956d92
Lincoln Neilsen <lincneilsen@gmail.com>
2026-01-12 11:20:12 -0700

    configure.js added

---
+++ src/config.js
@@ -0,0 +1,18 @@
+module.exports =  {
+    // Your JWT secret can be any random string you would like. It just needs to be secret.
+    jwtSecret: 'pizzaStore',
+    db: {
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'ThinkAboutTzu1020!',
+            database: 'pizza',
+            connectTimeout: 60000,
+        },
+        listPerPage: 10,
+    },
+    factory: {
+        url: 'https://pizza-factory.cs329.click',
+        apiKey: '0f6b9033a28d49f897be1313a52322cd',
+    },
+};
