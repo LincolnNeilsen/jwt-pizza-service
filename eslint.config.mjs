b43b1449a504e144b467d11b6a1f9e85bf0fc417
Lincoln Neilsen <lincneilsen@gmail.com>
2026-01-26 16:44:10 -0700

    config fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,18 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      semi: "error",
+      quotes: ["error", "single"],
+    },
+  },
+];
